9f5323aca94ed7c8dc935578eb4adca383d84c61
Tim Clifford <timothy.clifford@sap.com>
2022-01-10 22:22:44 +0100

    Until Page 83

--- stacks/index.js
+++ stacks/index.js
@@ -1,3 +1,6 @@
+import ApiStack from "./ApiStack";
 import StorageStack from "./StorageStack";
-export default function main(app) { new StorageStack(app, "storage");
-}
+export default function main(app) {
+  const storageStack = new StorageStack(app, "storage");
+  new ApiStack(app, "api", { table: storageStack.table,
+  }); }
