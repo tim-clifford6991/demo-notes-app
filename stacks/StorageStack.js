cea02c70a78412a0b8b4dc59cac48788c795e0cf
Tim Clifford <timothy.clifford@sap.com>
2022-01-10 21:40:35 +0100

    Second Commit

---
+++ stacks/StorageStack.js
@@ -0,0 +1,10 @@
+import * as sst from "@serverless-stack/resources";
+export default class StorageStack extends sst.Stack { // Public reference to the table
+table;
+constructor(scope, id, props) { super(scope, id, props);
+    // Create the DynamoDB table
+this.table = new sst.Table(this, "Notes", { fields: {
+userId: sst.TableFieldType.STRING,
+noteId: sst.TableFieldType.STRING, },
+primaryIndex: { partitionKey: "userId", sortKey: "noteId" }, });
+} }
--- stacks/index.js
+++ stacks/index.js
@@ -1,12 +1,3 @@
-import MyStack from "./MyStack";
-
-export default function main(app) {
-  // Set default runtime for all functions
-  app.setDefaultFunctionProps({
-    runtime: "nodejs12.x"
-  });
-
-  new MyStack(app, "my-stack");
-
-  // Add more stacks
+import StorageStack from "./StorageStack";
+export default function main(app) { new StorageStack(app, "storage");
 }
