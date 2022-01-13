618efa790d10fa56f667a430f81747e3dbdbafd3
Tim Clifford <timothy.clifford@sap.com>
2022-01-13 21:14:31 +0100

    Adding the API

--- stacks/StorageStack.js
+++ stacks/StorageStack.js
@@ -2,12 +2,20 @@ import * as sst from "@serverless-stack/resources";
 export default class StorageStack extends sst.Stack { // Public reference to the table
     bucket;
     table;
-constructor(scope, id, props) { super(scope, id, props);
-     // Create an S3 bucket
-this.bucket = new sst.Bucket(this, "Uploads");
-    // Create the DynamoDB table
-this.table = new sst.Table(this, "Notes", { fields: {
-userId: sst.TableFieldType.STRING,
-noteId: sst.TableFieldType.STRING, },
-primaryIndex: { partitionKey: "userId", sortKey: "noteId" }, });
-} }
+    constructor(scope, id, props) {
+        super(scope, id, props);
+        // Create an S3 bucket
+        this.bucket = new sst.Bucket(this, "Uploads");
+        // Create the DynamoDB table
+        this.table = new sst.Table(this, "Notes", {
+            fields: {
+                userId: sst.TableFieldType.STRING,
+                noteId: sst.TableFieldType.STRING,
+            },
+            primaryIndex: {
+                partitionKey: "userId",
+                sortKey: "noteId"
+            },
+        });
+    }
+}
--- stacks/index.js
+++ stacks/index.js
@@ -2,5 +2,7 @@ import ApiStack from "./ApiStack";
 import StorageStack from "./StorageStack";
 export default function main(app) {
   const storageStack = new StorageStack(app, "storage");
-  new ApiStack(app, "api", { table: storageStack.table,
-  }); }
+  new ApiStack(app, "api", {
+    table: storageStack.table,
+  });
+}
