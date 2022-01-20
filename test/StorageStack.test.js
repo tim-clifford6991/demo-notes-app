3bd6784382876face402f90f7a88f8b9a8533270
Tim Clifford <timothy.clifford@sap.com>
2022-01-20 13:26:58 +0100

    Completing Routes in React

--- test/StorageStack.test.js
+++ test/StorageStack.test.js
@@ -1,14 +1,13 @@
-import {
-    expect,
-    haveResource
-} from "@aws-cdk/assert";
+import { expect, haveResource } from "@aws-cdk/assert";
 import * as sst from "@serverless-stack/resources";
 import StorageStack from "../stacks/StorageStack";
 test("Test StorageStack", () => {
-    const app = new sst.App();
-    // WHEN
-    const stack = new StorageStack(app, "test-stack"); // THEN
-    expect(stack).to(haveResource("AWS::DynamoDB::Table", {
-        BillingMode: "PAY_PER_REQUEST",
-    }));
+  const app = new sst.App();
+  // WHEN
+  const stack = new StorageStack(app, "test-stack"); // THEN
+  expect(stack).to(
+    haveResource("AWS::DynamoDB::Table", {
+      BillingMode: "PAY_PER_REQUEST",
+    })
+  );
 });
