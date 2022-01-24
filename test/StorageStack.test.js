e5fcdfeab9d3a4d3a59c25532552fef555ef8c6f
Tim Clifford <timothy.clifford@sap.com>
2022-01-24 10:01:37 +0100

    Unit Test changes for CDK v2

--- test/StorageStack.test.js
+++ test/StorageStack.test.js
@@ -1,13 +1,22 @@
-import { template, haveResource } from "aws-cdk-lib/assertions";
+import { Template } from "aws-cdk-lib/assertions";
 import * as sst from "@serverless-stack/resources";
 import StorageStack from "../stacks/StorageStack";
 test("Test StorageStack", () => {
   const app = new sst.App();
-  // WHEN
   const stack = new StorageStack(app, "test-stack"); // THEN
-  template(stack).to(
-    haveResource("AWS::DynamoDB::Table", {
-      BillingMode: "PAY_PER_REQUEST",
-    })
-  );
+  const template = Template.fromStack(stack);
+
+  template.hasResource("AWS::DynamoDB::Table", {
+    BillingMode: "PAY_PER_REQUEST",
+  });
 });
+// const app = new sst.App();
+// // WHEN
+// const stack = new StorageStack(app, "test-stack"); // THEN
+// template(stack).to(
+//   haveResource("AWS::DynamoDB::Table", {
+//     BillingMode: "PAY_PER_REQUEST",
+//   })
+// );
+// };
+// );
