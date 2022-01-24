fc91fd63b8ee46e73d7cbc44adaadb091ee9bf0f
Tim Clifford <timothy.clifford@sap.com>
2022-01-24 10:10:40 +0100

    Final changes for CDKv2

--- test/StorageStack.test.js
+++ test/StorageStack.test.js
@@ -1,22 +1,14 @@
 import { Template } from "aws-cdk-lib/assertions";
 import * as sst from "@serverless-stack/resources";
 import StorageStack from "../stacks/StorageStack";
+
 test("Test StorageStack", () => {
   const app = new sst.App();
-  const stack = new StorageStack(app, "test-stack"); // THEN
+  // WHEN
+  const stack = new StorageStack(app, "test-stack");
+  // THEN
   const template = Template.fromStack(stack);
-
-  template.hasResource("AWS::DynamoDB::Table", {
+  template.hasResourceProperties("AWS::DynamoDB::Table", {
     BillingMode: "PAY_PER_REQUEST",
   });
 });
-// const app = new sst.App();
-// // WHEN
-// const stack = new StorageStack(app, "test-stack"); // THEN
-// template(stack).to(
-//   haveResource("AWS::DynamoDB::Table", {
-//     BillingMode: "PAY_PER_REQUEST",
-//   })
-// );
-// };
-// );
