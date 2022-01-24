31336c98d0499def4fd2c041bf8def932b0547f8
Tim Clifford <timothy.clifford@sap.com>
2022-01-24 09:54:13 +0100

    CDK v2 Changes

--- test/StorageStack.test.js
+++ test/StorageStack.test.js
@@ -1,11 +1,11 @@
-import { expect, haveResource } from "@aws-cdk/assert";
+import { template, haveResource } from "aws-cdk-lib/assertions";
 import * as sst from "@serverless-stack/resources";
 import StorageStack from "../stacks/StorageStack";
 test("Test StorageStack", () => {
   const app = new sst.App();
   // WHEN
   const stack = new StorageStack(app, "test-stack"); // THEN
-  expect(stack).to(
+  template(stack).to(
     haveResource("AWS::DynamoDB::Table", {
       BillingMode: "PAY_PER_REQUEST",
     })
